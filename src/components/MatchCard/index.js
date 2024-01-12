d21f24d14f05a376a79b8cad90ac37386ca51b0e
konadaprasad <151378851+konadaprasad@users.noreply.github.com>
2024-01-12 22:28:40 +0530

    Update index.js

--- src/components/MatchCard/index.js
+++ src/components/MatchCard/index.js
@@ -1,1 +1,28 @@
 // Write your code here
+import './index.css'
+
+const MatchCard = props => {
+  const {items} = props
+  const {result, competingTeam, competingTeamLogo, matchStatus} = items
+  let isWon
+  if (matchStatus === 'Won') {
+    isWon = true
+  } else {
+    isWon = false
+  }
+
+  return (
+    <li className="card-item">
+      <img
+        src={competingTeamLogo}
+        alt={`competing team ${competingTeam}`}
+        className="wonMatch-img"
+      />
+      <p className="head3">{competingTeam}</p>
+      <p className="p2">{result}</p>
+      <p className={isWon ? 'cardPara-green' : 'cardPara-red'}>{matchStatus}</p>
+    </li>
+  )
+}
+
+export default MatchCard
